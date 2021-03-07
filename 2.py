0b10d72566a1ea2eaf92b8e02f0d15cd55307686
mo1cy <79790607+mo1cy@users.noreply.github.com>
2021-03-07 15:57:11 +0300

    Delete 2.py

--- 2.py
+++
@@ -1,7 +0,0 @@
-program = 5000000 #pupils
-android = 70
-ios = 30
-pupils_for_android = program * 0.7
-pupils_for_ios = program * 0.3
-print("pupils for ios:" ,pupils_for_ios)
-print("pupils for android:" ,pupils_for_android)
