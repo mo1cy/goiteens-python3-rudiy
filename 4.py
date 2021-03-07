275694f85c4c9fde52d5ca60ef806a4f63b4aeee
mo1cy <79790607+mo1cy@users.noreply.github.com>
2021-03-07 15:57:35 +0300

    Delete 4.py

--- 4.py
+++
@@ -1,7 +0,0 @@
-loader = 25 #parcels
-parcel = 5 #kilograms
-car = 150 #kilograms was in car from start
-already_transfered = loader * parcel
-left_in_car = car - already_transfered #kilograms
-parcels_left_in_car = left_in_car / parcel
-print("parcels left in car:" ,parcels_left_in_car)
