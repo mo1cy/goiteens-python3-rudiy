169d86237087591e5055274ed49f30a7a1bad7ab
mo1cy <davydrudiy@gmail.com>
2021-03-07 16:21:07 +0300

    add new file

---
+++ 2.py
@@ -0,0 +1,7 @@
+program = 5000000 #pupils
+android = 70
+ios = 30
+pupils_for_android = program * 0.7
+pupils_for_ios = program * 0.3
+print("pupils for ios:" ,pupils_for_ios)
+print("pupils for android:" ,pupils_for_android)
